2194217b9a28e20a638b636b85a79bc9e9016be0
25kopek <dellinner@gmail.com>
2020-10-27 14:56:25 +0700

    brands carousel finished

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const emblaNode = document.getElementById("embla");
+const embla = EmblaCarousel(emblaNode, {align: "start", loop: "true"});
+
+let prevEmbla = document.getElementById('emblaButtonPrev');
+prevEmbla.addEventListener('click', embla.scrollPrev);
+
+let nextEmbla = document.getElementById('emblaButtonNext');
+nextEmbla.addEventListener('click', embla.scrollNext);
+
+
+const autoplay = (embla, interval) => {
+    let timer = 0;
+
+    const play = () => {
+        stop();
+        requestAnimationFrame(() => (timer = window.setTimeout(next, interval)));
+    };
+
+    const stop = () => {
+        window.clearTimeout(timer);
+        timer = 0;
+    };
+
+    const next = () => {
+        if (embla.canScrollNext()) {
+            embla.scrollNext();
+        } else {
+            embla.scrollTo(0);
+        }
+        play();
+    };
+
+    return { play, stop };
+};
+
+const autoplayer = autoplay(embla, 3000);
+
+embla.on("pointerDown", autoplayer.stop);
+embla.on("init", autoplayer.play);
